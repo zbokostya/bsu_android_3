b7273745cd6f9b00ed66850f6a106ae4c9d6cc94
zbokostya <kostya.zboichik@gmail.com>
2020-11-06 16:59:01 +0300

    fix bugs

--- Lab1/app/src/main/java/com/example/lab1/Form2.java
+++ Lab1/app/src/main/java/com/example/lab1/Form2.java
@@ -2,6 +2,7 @@ package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,15 +15,16 @@ public class Form2 extends AppCompatActivity {
     TextView counter;
     EditText editText;
 
+    String strExtra = "counter";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         counter = findViewById(R.id.counter);
-        String str = getIntent().getStringExtra("counter");
-        if (getIntent().getStringExtra("counter") != null) {
-            Log.d("123", str);
+        String str = getIntent().getStringExtra(strExtra);
+        if (getIntent().getStringExtra(strExtra) != null) {
             counter.setText(str);
         }
 
@@ -30,6 +32,7 @@ public class Form2 extends AppCompatActivity {
         nextForm.setOnClickListener(oclBtnOpenForm2);
 
         editText = findViewById(R.id.enterField);
+        editText.setText("");
 
 
     }
@@ -37,14 +40,13 @@ public class Form2 extends AppCompatActivity {
     View.OnClickListener oclBtnOpenForm2 = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            if (NumberUtility.isNumber(editText.getText().toString())) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                int val = Integer.parseInt(editText.getText().toString())
-                        + Integer.parseInt(counter.getText().toString());
-                intent.putExtra("counter", val + "");
-                startActivity(intent);
-            }
-
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            int val = Integer.parseInt(editText.getText().toString())
+                    + Integer.parseInt(counter.getText().toString());
+            intent.putExtra(strExtra, val + "");
+            editText.setText("");
+            setResult(Activity.RESULT_OK, intent);
+            finish();
         }
     };
 
--- Lab1/app/src/main/java/com/example/lab1/MainActivity.java
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     TextView counter;
     EditText editText;
 
+    int REQUEST_CODE = 100;
+    String strExtra = "counter";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +25,12 @@ public class MainActivity extends AppCompatActivity {
         counter = findViewById(R.id.counter);
 
         if (getIntent().getStringExtra("counter") != null) {
-            counter.setText(getIntent().getStringExtra("counter"));
+            counter.setText(getIntent().getStringExtra(strExtra));
         }
 
 
         editText = findViewById(R.id.enterField);
+
 
         Button nextForm = findViewById(R.id.nextFormButton);
         nextForm.setOnClickListener(oclBtnOpenForm2);
@@ -33,17 +38,29 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                String result = data.getStringExtra(strExtra);
+                TextView editor = findViewById(R.id.counter);
+                editor.setText(result);
+
+            }
+        }
+    }
+
 
     View.OnClickListener oclBtnOpenForm2 = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            if (NumberUtility.isNumber(editText.getText().toString())) {
-                Intent intent = new Intent(getApplicationContext(), Form2.class);
-                int val = Integer.parseInt(editText.getText().toString())
-                        + Integer.parseInt(counter.getText().toString());
-                intent.putExtra("counter", val + "");
-                startActivity(intent);
-            }
+            Intent intent = new Intent(getApplicationContext(), Form2.class);
+            int val = Integer.parseInt(editText.getText().toString())
+                    + Integer.parseInt(counter.getText().toString());
+            intent.putExtra("counter", val + "");
+            editText.setText("");
+            startActivityForResult(intent, REQUEST_CODE);
 
         }
     };
